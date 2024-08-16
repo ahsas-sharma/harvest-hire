a11ea7d386eda021b8c2bed6e9b70ad4b032baed
ahsas-sharma <sharma.ahsas@gmail.com>
2024-08-16 13:43:06 +0530

    Update config for production

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -20,6 +20,22 @@ if (process.env.NODE_ENV == "development") {
     MONGO_URL: process.env.MONGO_URL,
   };
 } else if (process.env.NODE_ENV == "production") {
+  CONFIG = {
+    PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV,
+    BASE_URL: "https://harvest-hire.onrender.com",
+    EMAIL_SMTP: {
+      MAILSENDER_API_KEY: process.env.MAILSENDER_API_KEY,
+      MAILSENDER_EMAIL: process.env.MAILSENDER_EMAIL,
+      MAILSENDER_NAME: process.env.MAILSENDER_NAME,
+    },
+    TWILIO_SMS: {
+      ACC_SID: process.env.TWILIO_SID,
+      TOKEN: process.env.TWILIO_TOKEN,
+    },
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+    MONGO_URL: process.env.MONGO_URL,
+  };
 }
 
 export default CONFIG;
